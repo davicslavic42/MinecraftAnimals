973378698c697287c02f1652e081cc6bccbe315d
davicslavic42 <52937478+davicslavic42@users.noreply.github.com>
2020-07-04 01:57:51 -0700

    Add files via upload

---
+++ Items/Blocks/Endstone.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace MinecraftAnimals.Items.Blocks
+{
+	public class Endstone : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("This is a modded block.");
+			ItemID.Sets.ExtractinatorMode[item.type] = item.type;
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 16;
+			item.height = 16;
+			item.maxStack = 999;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = 1;
+			item.consumable = true;
+			item.createTile = TileType<Tiles.Endstonetile>();
+		}
+	}
+}
