8bd7ca39b8f18224c9b68ca9803d1b182111c854
davicslavic42 <52937478+davicslavic42@users.noreply.github.com>
2020-07-04 01:40:04 -0700

    Add files via upload

---
+++ Items/ShadeShroom.cs
@@ -0,0 +1,39 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using MinecraftAnimals.Mounts;
+using Microsoft.Xna.Framework;
+
+namespace MinecraftAnimals.Items
+{
+	public class ShadeShroom : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("Summons a rideable mooshroom");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 20;
+			item.height = 30;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.value = 30000;
+			item.rare = ItemRarityID.Green;
+			item.UseSound = SoundID.Item79;
+			item.noMelee = true;
+			item.mountType = MountType<MooshroomMount>();
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Mushroom, 10);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
