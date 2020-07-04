956c46fcff9bc95051cd933227f88d3f39f140fa
davicslavic42 <52937478+davicslavic42@users.noreply.github.com>
2020-07-04 01:36:10 -0700

    Add files via upload

---
+++ Mounts/MooshroomMount.cs
@@ -0,0 +1,62 @@
+﻿using Terraria;
+using MinecraftAnimals.Buffs;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace MinecraftAnimals.Mounts
+{
+	public class MooshroomMount : ModMountData
+	{
+		public override void SetDefaults()
+		{
+			mountData.heightBoost = 20;
+			mountData.fallDamage = 0.1f;
+			mountData.runSpeed = 10f;
+			mountData.dashSpeed = 8f;
+			mountData.flightTimeMax = 0;
+			mountData.fatigueMax = 0;
+			mountData.buff = BuffType<MooshroomMountBuff>();
+			mountData.jumpHeight = 5;
+			mountData.acceleration = 0.25f;
+			mountData.jumpSpeed = 5f;
+			mountData.blockExtraJumps = false;
+			mountData.totalFrames = 8;
+			mountData.constantJump = true;
+			int[] array = new int[mountData.totalFrames];
+			for (int l = 0; l < array.Length; l++)
+			{
+				array[l] = 20;
+			}
+			mountData.playerYOffsets = array;
+			mountData.xOffset = 13;
+			mountData.bodyFrame = 3;
+			mountData.yOffset = -12;
+			mountData.playerHeadOffset = 22;
+			mountData.standingFrameCount = 4;
+			mountData.standingFrameDelay = 12;
+			mountData.standingFrameStart = 0;
+			mountData.runningFrameCount = 4;
+			mountData.runningFrameDelay = 12;
+			mountData.runningFrameStart = 0;
+			mountData.flyingFrameCount = 0;
+			mountData.flyingFrameDelay = 0;
+			mountData.flyingFrameStart = 0;
+			mountData.inAirFrameCount = 1;
+			mountData.inAirFrameDelay = 12;
+			mountData.inAirFrameStart = 0;
+			mountData.idleFrameCount = 4;
+			mountData.idleFrameDelay = 12;
+			mountData.idleFrameStart = 0;
+			mountData.idleFrameLoop = true;
+			mountData.swimFrameCount = mountData.inAirFrameCount;
+			mountData.swimFrameDelay = mountData.inAirFrameDelay;
+			mountData.swimFrameStart = mountData.inAirFrameStart;
+			if (Main.netMode == NetmodeID.Server)
+			{
+				return;
+			}
+		}
+	}
+}
+
