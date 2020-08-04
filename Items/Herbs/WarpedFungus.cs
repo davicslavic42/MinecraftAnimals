82e3f09ed9eabc8872fdad6946537594419712c8
davicslavic42 <wwwdavidn817@gmail.com>
2020-08-04 15:08:07 -0700

    next update

---
+++ Items/Herbs/WarpedFungus.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace MinecraftAnimals.Items.Herbs
+{
+	public class WarpedFungus : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("A blueish fungus found in the underworlds");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 12;
+			item.height = 12;
+			item.maxStack = 999;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.consumable = true;
+		}
+	}
+}
