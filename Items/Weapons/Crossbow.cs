e8bc2ccef12cbc5ce5b49047ce9cd5598689dc8b
dav <wwwdavidn817@gmail.com>
2021-02-27 22:26:32 -0800

    pre v0.1.2.1

--- Items/Weapons/Crossbow.cs
+++ Items/Weapons/Crossbow.cs
@@ -24,7 +24,7 @@ namespace MinecraftAnimals.Items.Weapons
             // Alter any of these values as you see fit, but you should probably keep useStyle on 1, as well as the noUseGraphic and noMelee bools
             item.shootSpeed = 18f;
             item.damage = 38;
-            item.knockBack = 5f;
+            item.knockBack = 6f;
             //Channel so that you can held the weapon [Important]  item.channel = true;
             item.useStyle = ItemUseStyleID.HoldingOut;
             item.useAnimation = 100;
